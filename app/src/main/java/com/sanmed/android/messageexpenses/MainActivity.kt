2cae6841f9b677d271633c680355feccc8e83528
Santiago Medina <santiago@timba.co>
2019-10-20 16:43:54 -0500

    Start working with regex

--- app/src/main/java/com/sanmed/android/messageexpenses/MainActivity.kt
+++ app/src/main/java/com/sanmed/android/messageexpenses/MainActivity.kt
@@ -3,49 +3,69 @@ package com.sanmed.android.messageexpenses
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.net.Uri
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import android.text.method.ScrollingMovementMethod
 
 
-class MainActivity : AppCompatActivity(), View.OnClickListener  {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnLoadSMS.setOnClickListener(this)
-
+        smsTexts.movementMethod = ScrollingMovementMethod()
     }
 
     override fun onClick(v: View?) {
-        if(v?.id == R.id.btnLoadSMS){
+        if (v?.id == R.id.btnLoadSMS) {
 
-            if(ContextCompat.checkSelfPermission(baseContext, "android.permission.READ_SMS") == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(
+                    baseContext,
+                    "android.permission.READ_SMS"
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
                 loadSMS()
-            }else{
+            } else {
                 ActivityCompat.requestPermissions(this, arrayOf("android.permission.READ_SMS"), 123)
             }
 
 
-
         }
     }
-    fun loadSMS(){
+
+    private fun loadSMS() {
         smsTexts.text = "No SMS"
         contentResolver?.query(Uri.parse("content://sms/inbox"), null, null, null, null)?.use {
-            if (it.moveToFirst()) { // must check the result to prevent exception
-                do {
-                    var msgData = ""
-                    for (idx in 0 until it.columnCount) {
-                        msgData += " " + it.getColumnName(idx) + ":" + it.getString(idx)
-                    }
-                    // use msgData
-                    smsTexts.text = msgData
-                } while (it.moveToNext())
-            }
+            var resultData = String.format("Total messages %d \n", it.count)
+            resultData += "Gastos"
+            var msgData: String
+            it.moveToFirst()
+            do {
+                msgData = it.getString(it.getColumnIndex("body"))
+                if (msgData.contains("Bancolombia le informa Compra por")) {
+                    resultData += "\n\n\n"
+                    resultData += msgData
+                    resultData += getPurchaseDetails(msgData)
+                }
+            } while (it.moveToNext())
+            smsTexts.text = resultData
         }
+    }
+
+    private fun getPurchaseDetails(message: String):String {
+        val regexValue = "\\$[\\d.,']+".toRegex()
+        val regexSite = " [\\w\\s]+ \\d{2}:\\d{2}".toRegex()
+
+        var result = ""
+        result +="\nDetails"
+        var match = regexValue.find(message)
+        result += String.format("\n Valor: %s",match?.value)
+        match = regexSite.find(message)
+        result += String.format("\n Lugar: %s",match?.value)
+        return result
     }
 }
